d133a5b214e30c73a4ee72148231a94092734cea
Olivier <olivier.bondu@gmail.com>
2015-07-23 18:02:38 +0200

    Updating gen level info

--- plugins/HHAnalyzer.cc
+++ plugins/HHAnalyzer.cc
@@ -32,43 +32,68 @@ void HHAnalyzer::analyze(const edm::Event&, const edm::EventSetup&, const Produc
     LorentzVector gen_llmet(0.,0.,0.,0.);
     LorentzVector gen_llFSRmet(0.,0.,0.,0.);
 */
-    int iHiggs1, iHiggs2;
-    iHiggs1 = iHiggs2 = -1;
-/*
+    int iH1, iH2;
     int iB1, iB2;
-    int iJet1, iJet2;
     int iL1, iL2;
     int iNu1, iNu2;
+    iH1 = iH2 = -1;
     iB1 = iB2 = -1;
-    iJet1 = iJet2 = -1;
     iL1 = iL2 = -1;
     iNu1 = iNu2 = -1;
-*/
-    // Construct the signal gen-level info
-    // Get the Higgses !
-    for (unsigned int ip = 0 ; ip < gp.pruned_p4.size() ; ip++) {
-        if( gp.pruned_pdg_id[ip] != 25 ) continue; // just take care of the Higgses for now
-        std::bitset<15> flags (gp.pruned_status_flags[ip]);
-        if( flags.test(13) ) continue; // must be the last copy
-        if( iHiggs1 == -1 )
-            iHiggs1 = ip;
-        else if( iHiggs2 == -1 )
-            iHiggs2 = ip;
-        else
-            std::cout << "ERROR, more than 3 Higgses in the event!" << std::endl;
-    }
-    std::cout << "\tiHiggs1= " << iHiggs1 << "\tgp.pruned_pdg_id= " << gp.pruned_pdg_id[iHiggs1] << "\tflags= " << std::bitset<15>(gp.pruned_status_flags[iHiggs1]) << "\t(pt, eta, phi, e, mass)= (" << gp.pruned_p4[iHiggs1].Pt() << " , " << gp.pruned_p4[iHiggs1].Eta() << " , " << gp.pruned_p4[iHiggs1].Phi() << " , " << gp.pruned_p4[iHiggs1].E() << " , " << gp.pruned_p4[iHiggs1].M() << ")" << std::endl;
-    std::cout << "\tiHiggs2= " << iHiggs2 << "\tgp.pruned_pdg_id= " << gp.pruned_pdg_id[iHiggs2] << "\tflags= " << std::bitset<15>(gp.pruned_status_flags[iHiggs2]) << "\t(pt, eta, phi, e, mass)= (" << gp.pruned_p4[iHiggs2].Pt() << " , " << gp.pruned_p4[iHiggs2].Eta() << " , " << gp.pruned_p4[iHiggs2].Phi() << " , " << gp.pruned_p4[iHiggs2].E() << " , " << gp.pruned_p4[iHiggs2].M() << ")" << std::endl;
 
-    // Get the Higgses decay products
+    // Construct the signal gen-level info
     for (unsigned int ip = 0 ; ip < gp.pruned_p4.size() ; ip++) {
-//        if( gp.pruned_pdg_id[ip] == 25 ) continue; // skip the Higgs this time
         std::bitset<15> flags (gp.pruned_status_flags[ip]);
         if( !(flags.test(13) && flags.test(8)) ) continue; // take the last copies coming from the hard process
-        std::cout << "\t\tip= " << ip << "\tgp.pruned_pdg_id= " << gp.pruned_pdg_id[ip] << "\tflags= " << flags << "\t(pt, eta, phi, e, mass)= (" << gp.pruned_p4[ip].Pt() << " , " << gp.pruned_p4[ip].Eta() << " , " << gp.pruned_p4[ip].Phi() << " , " << gp.pruned_p4[ip].E() << " , " << gp.pruned_p4[ip].M() << ")" << std::endl;
-        for(unsigned int imother = 0 ; imother < gp.pruned_mothers_index[ip].size() ; imother++)
-            std::cout << "\t\t\timother= " << imother << "\tindex= " << gp.pruned_mothers_index[ip][imother] << std::endl;
-    }
+        if( abs(gp.pruned_pdg_id[ip]) == 25 )
+        {
+            if( iH1 == -1 )
+                iH1 = ip;
+            else if( iH2 == -1 )
+                iH2 = ip;
+            else
+                std::cout << "ERROR, more than 2 Higgs in the hard process!" << std::endl;
+        }
+        else if( abs(gp.pruned_pdg_id[ip]) == 5 )
+        {
+            if( iB1 == -1 )
+                iB1 = ip;
+            else if( iB2 == -1 )
+                iB2 = ip;
+            else
+                std::cout << "ERROR, more than 2 bjets in the hard process!" << std::endl;
+        }
+        else if( abs(gp.pruned_pdg_id[ip]) == 11 || abs(gp.pruned_pdg_id[ip]) == 13 )
+        {
+            if( iL1 == -1 )
+                iL1 = ip;
+            else if( iL2 == -1 )
+                iL2 = ip;
+            else
+                std::cout << "ERROR, more than 2 leptons in the hard process!" << std::endl;
+        }
+        else if( abs(gp.pruned_pdg_id[ip]) == 12 || abs(gp.pruned_pdg_id[ip]) == 14 || abs(gp.pruned_pdg_id[ip]) == 16 )
+        {
+            if( iNu1 == -1 )
+                iNu1 = ip;
+            else if( iNu2 == -1 )
+                iNu2 = ip;
+            else
+                std::cout << "ERROR, more than 2 neutrinos in the hard process!" << std::endl;
+        }
+//FIXME if the lepton is not in the hard process itself, then look for FSR photons
+//FIXME keep radion / graviton / W / Z when present
+    } // end of loop over pruned gen particles
+    std::cout << "\tiH1= " << iH1 << "\tgp.pruned_pdg_id= " << gp.pruned_pdg_id[iH1] << "\tflags= " << std::bitset<15>(gp.pruned_status_flags[iH1]) << "\t(pt, eta, phi, e, mass)= (" << gp.pruned_p4[iH1].Pt() << " , " << gp.pruned_p4[iH1].Eta() << " , " << gp.pruned_p4[iH1].Phi() << " , " << gp.pruned_p4[iH1].E() << " , " << gp.pruned_p4[iH1].M() << ")" << std::endl;
+    std::cout << "\tiH2= " << iH2 << "\tgp.pruned_pdg_id= " << gp.pruned_pdg_id[iH2] << "\tflags= " << std::bitset<15>(gp.pruned_status_flags[iH2]) << "\t(pt, eta, phi, e, mass)= (" << gp.pruned_p4[iH2].Pt() << " , " << gp.pruned_p4[iH2].Eta() << " , " << gp.pruned_p4[iH2].Phi() << " , " << gp.pruned_p4[iH2].E() << " , " << gp.pruned_p4[iH2].M() << ")" << std::endl;
+    std::cout << "\tiB1= " << iB1 << "\tgp.pruned_pdg_id= " << gp.pruned_pdg_id[iB1] << "\tflags= " << std::bitset<15>(gp.pruned_status_flags[iB1]) << "\t(pt, eta, phi, e, mass)= (" << gp.pruned_p4[iB1].Pt() << " , " << gp.pruned_p4[iB1].Eta() << " , " << gp.pruned_p4[iB1].Phi() << " , " << gp.pruned_p4[iB1].E() << " , " << gp.pruned_p4[iB1].M() << ")" << std::endl;
+    std::cout << "\tiB2= " << iB2 << "\tgp.pruned_pdg_id= " << gp.pruned_pdg_id[iB2] << "\tflags= " << std::bitset<15>(gp.pruned_status_flags[iB2]) << "\t(pt, eta, phi, e, mass)= (" << gp.pruned_p4[iB2].Pt() << " , " << gp.pruned_p4[iB2].Eta() << " , " << gp.pruned_p4[iB2].Phi() << " , " << gp.pruned_p4[iB2].E() << " , " << gp.pruned_p4[iB2].M() << ")" << std::endl;
+    std::cout << "\tiL1= " << iL1 << "\tgp.pruned_pdg_id= " << gp.pruned_pdg_id[iL1] << "\tflags= " << std::bitset<15>(gp.pruned_status_flags[iL1]) << "\t(pt, eta, phi, e, mass)= (" << gp.pruned_p4[iL1].Pt() << " , " << gp.pruned_p4[iL1].Eta() << " , " << gp.pruned_p4[iL1].Phi() << " , " << gp.pruned_p4[iL1].E() << " , " << gp.pruned_p4[iL1].M() << ")" << std::endl;
+    std::cout << "\tiL2= " << iL2 << "\tgp.pruned_pdg_id= " << gp.pruned_pdg_id[iL2] << "\tflags= " << std::bitset<15>(gp.pruned_status_flags[iL2]) << "\t(pt, eta, phi, e, mass)= (" << gp.pruned_p4[iL2].Pt() << " , " << gp.pruned_p4[iL2].Eta() << " , " << gp.pruned_p4[iL2].Phi() << " , " << gp.pruned_p4[iL2].E() << " , " << gp.pruned_p4[iL2].M() << ")" << std::endl;
+    std::cout << "\tiNu1= " << iNu1 << "\tgp.pruned_pdg_id= " << gp.pruned_pdg_id[iNu1] << "\tflags= " << std::bitset<15>(gp.pruned_status_flags[iNu1]) << "\t(pt, eta, phi, e, mass)= (" << gp.pruned_p4[iNu1].Pt() << " , " << gp.pruned_p4[iNu1].Eta() << " , " << gp.pruned_p4[iNu1].Phi() << " , " << gp.pruned_p4[iNu1].E() << " , " << gp.pruned_p4[iNu1].M() << ")" << std::endl;
+    std::cout << "\tiNu2= " << iNu2 << "\tgp.pruned_pdg_id= " << gp.pruned_pdg_id[iNu2] << "\tflags= " << std::bitset<15>(gp.pruned_status_flags[iNu2]) << "\t(pt, eta, phi, e, mass)= (" << gp.pruned_p4[iNu2].Pt() << " , " << gp.pruned_p4[iNu2].Eta() << " , " << gp.pruned_p4[iNu2].Phi() << " , " << gp.pruned_p4[iNu2].E() << " , " << gp.pruned_p4[iNu2].M() << ")" << std::endl;
+
+
 /*
     for (unsigned int ip = 0 ; ip < gp.pruned_p4.size() ; ip++) {
         std::bitset<15> flags (gp.pruned_status_flags[ip]);
