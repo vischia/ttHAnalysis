450669781e66cbe00c2cf2e06d01b2d6f729b20a
Brieuc Francois <brieucfrancois@hotmail.com>
2015-09-15 15:13:38 +0200

    Remove DiLep categories which are now in the fwk

--- interface/Categories.h
+++ interface/Categories.h
@@ -3,38 +3,6 @@
 
 #include <cp3_llbb/Framework/interface/Category.h>
 
-class MuMuCategory: public Category {
-    virtual bool event_in_category_pre_analyzers(const ProducersManager& producers) const override;
-    virtual bool event_in_category_post_analyzers(const ProducersManager& producers, const AnalyzersManager& analyzers) const override;
-    virtual void register_cuts(CutManager& manager) override;
-    virtual void evaluate_cuts_pre_analyzers(CutManager& manager, const ProducersManager& producers) const override;
-    virtual void evaluate_cuts_post_analyzers(CutManager& manager, const ProducersManager& producers, const AnalyzersManager& analyzers) const override;
-};
-
-class MuElCategory: public Category {
-    virtual bool event_in_category_pre_analyzers(const ProducersManager& producers) const override;
-    virtual bool event_in_category_post_analyzers(const ProducersManager& producers, const AnalyzersManager& analyzers) const override;
-    virtual void register_cuts(CutManager& manager) override;
-    virtual void evaluate_cuts_pre_analyzers(CutManager& manager, const ProducersManager& producers) const override;
-    virtual void evaluate_cuts_post_analyzers(CutManager& manager, const ProducersManager& producers, const AnalyzersManager& analyzers) const override;
-};
-
-class ElMuCategory: public Category {
-    virtual bool event_in_category_pre_analyzers(const ProducersManager& producers) const override;
-    virtual bool event_in_category_post_analyzers(const ProducersManager& producers, const AnalyzersManager& analyzers) const override;
-    virtual void register_cuts(CutManager& manager) override;
-    virtual void evaluate_cuts_pre_analyzers(CutManager& manager, const ProducersManager& producers) const override;
-    virtual void evaluate_cuts_post_analyzers(CutManager& manager, const ProducersManager& producers, const AnalyzersManager& analyzers) const override;
-};
-
-class ElElCategory: public Category {
-    virtual bool event_in_category_pre_analyzers(const ProducersManager& producers) const override;
-    virtual bool event_in_category_post_analyzers(const ProducersManager& producers, const AnalyzersManager& analyzers) const override;
-    virtual void register_cuts(CutManager& manager) override;
-    virtual void evaluate_cuts_pre_analyzers(CutManager& manager, const ProducersManager& producers) const override;
-    virtual void evaluate_cuts_post_analyzers(CutManager& manager, const ProducersManager& producers, const AnalyzersManager& analyzers) const override;
-};
-
 class DiJetCategory: public Category {
     virtual bool event_in_category_pre_analyzers(const ProducersManager& producers) const override;
     virtual bool event_in_category_post_analyzers(const ProducersManager& producers, const AnalyzersManager& analyzers) const override;
