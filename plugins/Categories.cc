e0eb9ef999bcf30f8cce374eee4a6839ca3a33fc
Olivier Bondu <olivier.bondu@gmail.com>
2015-10-06 17:45:43 +0200

    Refactoring, fixes #13

--- plugins/Categories.cc
+++ plugins/Categories.cc
@@ -12,181 +12,181 @@
 // Dilepton categories
 // ***** ***** *****
 
-const std::vector<lepton>& HHDileptonCategory::getLeptons(const AnalyzersManager& analyzers) const {
-    const HHAnalyzer& hhanalyzer = analyzers.get<HHAnalyzer>("hh_analyzer");
-    return hhanalyzer.Leptons;
+const std::vector<Lepton>& DileptonCategory::getLeptons(const AnalyzersManager& analyzers) const {
+    const HHAnalyzer& hh_analyzer = analyzers.get<HHAnalyzer>("hh_analyzer");
+    return hh_analyzer.leptons;
 }
 
-const std::vector<dilepton>& HHDileptonCategory::getdiLeptons(const AnalyzersManager& analyzers) const {
-    const HHAnalyzer& hhanalyzer = analyzers.get<HHAnalyzer>("hh_analyzer");
-    return hhanalyzer.diLeptons;
+const std::vector<Dilepton>& DileptonCategory::getDileptons(const AnalyzersManager& analyzers) const {
+    const HHAnalyzer& hh_analyzer = analyzers.get<HHAnalyzer>("hh_analyzer");
+    return hh_analyzer.ll;
 }
 
-const unsigned int HHDileptonCategory::getNumberOfJet(const AnalyzersManager& analyzers) const {
-    const HHAnalyzer& hhanalyzer = analyzers.get<HHAnalyzer>("hh_analyzer");
-    return hhanalyzer.selectedjets_p4.size();
+const unsigned int DileptonCategory::getNJets(const AnalyzersManager& analyzers) const {
+    const HHAnalyzer& hh_analyzer = analyzers.get<HHAnalyzer>("hh_analyzer");
+    return hh_analyzer.jets_p4.size();
 }
 
-const unsigned int HHDileptonCategory::getNumberOfBJet(const AnalyzersManager& analyzers) const {
-    const HHAnalyzer& hhanalyzer = analyzers.get<HHAnalyzer>("hh_analyzer");
-    return hhanalyzer.selectedbjets_p4.size();
+const unsigned int DileptonCategory::getNBJets(const AnalyzersManager& analyzers) const {
+    const HHAnalyzer& hh_analyzer = analyzers.get<HHAnalyzer>("hh_analyzer");
+    return hh_analyzer.bjets_p4.size();
 }
 
 // ***** ***** *****
 // Dilepton Mu-Mu category
 // ***** ***** *****
-bool HHMuMuCategory::event_in_category_pre_analyzers(const ProducersManager& producers) const {
+bool MuMuCategory::event_in_category_pre_analyzers(const ProducersManager& producers) const {
     const MuonsProducer& muons = producers.get<MuonsProducer>("muons");
     const JetsProducer& jets = producers.get<JetsProducer>("jets");
     return (muons.p4.size() >= 2 && jets.p4.size() >= 2);
 };
 
-bool HHMuMuCategory::event_in_category_post_analyzers(const ProducersManager& producers, const AnalyzersManager& analyzers) const {
-    const std::vector<dilepton>& tempdiLeptons = getdiLeptons(analyzers);
+bool MuMuCategory::event_in_category_post_analyzers(const ProducersManager& producers, const AnalyzersManager& analyzers) const {
+    const std::vector<Dilepton>& ll = getDileptons(analyzers);
     bool isMuMu = false;
-    for (unsigned int idilep = 0; idilep < tempdiLeptons.size(); idilep++) 
+    for (unsigned int idilep = 0; idilep < ll.size(); idilep++) 
     {
-        if (tempdiLeptons[idilep].isMuMu) isMuMu = true;
+        if (ll[idilep].isMuMu) isMuMu = true;
     }
-    const unsigned int numberOfJet = getNumberOfJet(analyzers);
-    return (isMuMu && numberOfJet >= 2);
+    const unsigned int nJets = getNJets(analyzers);
+    return (isMuMu && nJets >= 2);
 };
 
-void HHMuMuCategory::register_cuts(CutManager& manager) {
+void MuMuCategory::register_cuts(CutManager& manager) {
     manager.new_cut("ll_mass", "mll > 20");
     manager.new_cut("ll_mass_lowerZcut", "mll > 85");
     manager.new_cut("has_two_bJets", "nBJet >= 2");
 };
 
-void HHMuMuCategory::evaluate_cuts_post_analyzers(CutManager& manager, const ProducersManager& producers, const AnalyzersManager& analyzers) const {
-    const std::vector<dilepton>& tempdiLeptons = getdiLeptons(analyzers);
-    for (unsigned int idilep = 0; idilep < tempdiLeptons.size(); idilep++) 
+void MuMuCategory::evaluate_cuts_post_analyzers(CutManager& manager, const ProducersManager& producers, const AnalyzersManager& analyzers) const {
+    const std::vector<Dilepton>& ll = getDileptons(analyzers);
+    for (unsigned int idilep = 0; idilep < ll.size(); idilep++) 
     {
-        if ( tempdiLeptons[idilep].isMuMu ) {
-            if (  tempdiLeptons[idilep].p4.M() > m_mll_cut  ) manager.pass_cut("ll_mass");
-            if (  tempdiLeptons[idilep].p4.M() > m_mll_lowerZcut  ) manager.pass_cut("ll_mass_lowerZcut");
+        if ( ll[idilep].isMuMu ) {
+            if (  ll[idilep].p4.M() > m_mll_cut  ) manager.pass_cut("ll_mass");
+            if (  ll[idilep].p4.M() > m_mll_lowerZcut  ) manager.pass_cut("ll_mass_lowerZcut");
         }
     }
-    const unsigned int numberOfBJet = getNumberOfBJet(analyzers);
-    if ( numberOfBJet >=2 ) manager.pass_cut("has_two_bJets"); 
+    const unsigned int nBJets = getNBJets(analyzers);
+    if ( nBJets >=2 ) manager.pass_cut("has_two_bJets"); 
 }
 
 // ***** ***** *****
 // Dilepton El-El category
 // ***** ***** *****
-bool HHElElCategory::event_in_category_pre_analyzers(const ProducersManager& producers) const {
+bool ElElCategory::event_in_category_pre_analyzers(const ProducersManager& producers) const {
     const ElectronsProducer& electrons = producers.get<ElectronsProducer>("electrons");
     const JetsProducer& jets = producers.get<JetsProducer>("jets");
     return (electrons.p4.size() >= 2  && jets.p4.size() >= 2);
 };
 
-bool HHElElCategory::event_in_category_post_analyzers(const ProducersManager& producers, const AnalyzersManager& analyzers) const {
-    const std::vector<dilepton>& tempdiLeptons = getdiLeptons(analyzers);
+bool ElElCategory::event_in_category_post_analyzers(const ProducersManager& producers, const AnalyzersManager& analyzers) const {
+    const std::vector<Dilepton>& ll = getDileptons(analyzers);
     bool isElEl = false;
-    for (unsigned int idilep = 0; idilep < tempdiLeptons.size(); idilep++) 
+    for (unsigned int idilep = 0; idilep < ll.size(); idilep++) 
     {
-        if (tempdiLeptons[idilep].isElEl) isElEl = true;
+        if (ll[idilep].isElEl) isElEl = true;
     }
-    const unsigned int numberOfJet = getNumberOfJet(analyzers);
-    return (isElEl && numberOfJet >= 2);
+    const unsigned int nJets = getNJets(analyzers);
+    return (isElEl && nJets >= 2);
 };
 
-void HHElElCategory::register_cuts(CutManager& manager) {
+void ElElCategory::register_cuts(CutManager& manager) {
     manager.new_cut("ll_mass", "mll > 20");
     manager.new_cut("ll_mass_lowerZcut", "mll > 85");
     manager.new_cut("has_two_bJets", "nBJet >= 2");
 };
 
-void HHElElCategory::evaluate_cuts_post_analyzers(CutManager& manager, const ProducersManager& producers, const AnalyzersManager& analyzers) const {
-    const std::vector<dilepton>& tempdiLeptons = getdiLeptons(analyzers);
-    for (unsigned int idilep = 0; idilep < tempdiLeptons.size(); idilep++) 
+void ElElCategory::evaluate_cuts_post_analyzers(CutManager& manager, const ProducersManager& producers, const AnalyzersManager& analyzers) const {
+    const std::vector<Dilepton>& ll = getDileptons(analyzers);
+    for (unsigned int idilep = 0; idilep < ll.size(); idilep++) 
     {
-        if ( tempdiLeptons[idilep].isElEl ) {
-            if (  tempdiLeptons[idilep].p4.M() > m_mll_cut  ) manager.pass_cut("ll_mass");
-            if (  tempdiLeptons[idilep].p4.M() > m_mll_lowerZcut  ) manager.pass_cut("ll_mass_lowerZcut");
+        if ( ll[idilep].isElEl ) {
+            if (  ll[idilep].p4.M() > m_mll_cut  ) manager.pass_cut("ll_mass");
+            if (  ll[idilep].p4.M() > m_mll_lowerZcut  ) manager.pass_cut("ll_mass_lowerZcut");
         }
     }
-    const unsigned int numberOfBJet = getNumberOfBJet(analyzers);
-    if ( numberOfBJet >=2 ) manager.pass_cut("has_two_bJets"); 
+    const unsigned int nBJets = getNBJets(analyzers);
+    if ( nBJets >=2 ) manager.pass_cut("has_two_bJets"); 
 }
 
 // ***** ***** *****
 // Dilepton El-Mu category
 // ***** ***** *****
-bool HHElMuCategory::event_in_category_pre_analyzers(const ProducersManager& producers) const {
+bool ElMuCategory::event_in_category_pre_analyzers(const ProducersManager& producers) const {
     const ElectronsProducer& electrons = producers.get<ElectronsProducer>("electrons");
     const MuonsProducer& muons = producers.get<MuonsProducer>("muons");
     const JetsProducer& jets = producers.get<JetsProducer>("jets");
     return ((electrons.p4.size() + muons.p4.size() >= 2)  && jets.p4.size() >= 2);
 };
 
-bool HHElMuCategory::event_in_category_post_analyzers(const ProducersManager& producers, const AnalyzersManager& analyzers) const {
-    const std::vector<dilepton>& tempdiLeptons = getdiLeptons(analyzers);
+bool ElMuCategory::event_in_category_post_analyzers(const ProducersManager& producers, const AnalyzersManager& analyzers) const {
+    const std::vector<Dilepton>& ll = getDileptons(analyzers);
     bool isElMu = false;
-    for (unsigned int idilep = 0; idilep < tempdiLeptons.size(); idilep++) 
+    for (unsigned int idilep = 0; idilep < ll.size(); idilep++) 
     {
-        if (tempdiLeptons[idilep].isElMu) isElMu = true;
+        if (ll[idilep].isElMu) isElMu = true;
     }
-    const unsigned int numberOfJet = getNumberOfJet(analyzers);
-    return (isElMu && numberOfJet >= 2);
+    const unsigned int nJets = getNJets(analyzers);
+    return (isElMu && nJets >= 2);
 };
 
-void HHElMuCategory::register_cuts(CutManager& manager) {
+void ElMuCategory::register_cuts(CutManager& manager) {
     manager.new_cut("ll_mass", "mll > 20");
     manager.new_cut("ll_mass_lowerZcut", "mll > 85");
     manager.new_cut("has_two_bJets", "nBJet >= 2");
 };
 
-void HHElMuCategory::evaluate_cuts_post_analyzers(CutManager& manager, const ProducersManager& producers, const AnalyzersManager& analyzers) const {
-    const std::vector<dilepton>& tempdiLeptons = getdiLeptons(analyzers);
-    for (unsigned int idilep = 0; idilep < tempdiLeptons.size(); idilep++) 
+void ElMuCategory::evaluate_cuts_post_analyzers(CutManager& manager, const ProducersManager& producers, const AnalyzersManager& analyzers) const {
+    const std::vector<Dilepton>& ll = getDileptons(analyzers);
+    for (unsigned int idilep = 0; idilep < ll.size(); idilep++) 
     {
-        if ( tempdiLeptons[idilep].isElMu ) {
-            if (  tempdiLeptons[idilep].p4.M() > m_mll_cut  ) manager.pass_cut("ll_mass");
-            if (  tempdiLeptons[idilep].p4.M() > m_mll_lowerZcut  ) manager.pass_cut("ll_mass_lowerZcut");
+        if ( ll[idilep].isElMu ) {
+            if (  ll[idilep].p4.M() > m_mll_cut  ) manager.pass_cut("ll_mass");
+            if (  ll[idilep].p4.M() > m_mll_lowerZcut  ) manager.pass_cut("ll_mass_lowerZcut");
         }
     }
-    const unsigned int numberOfBJet = getNumberOfBJet(analyzers);
-    if ( numberOfBJet >=2 ) manager.pass_cut("has_two_bJets"); 
+    const unsigned int nBJets = getNBJets(analyzers);
+    if ( nBJets >=2 ) manager.pass_cut("has_two_bJets"); 
 }
 
 // ***** ***** *****
 // Dilepton Mu-El category
 // ***** ***** *****
-bool HHMuElCategory::event_in_category_pre_analyzers(const ProducersManager& producers) const {
+bool MuElCategory::event_in_category_pre_analyzers(const ProducersManager& producers) const {
     const ElectronsProducer& electrons = producers.get<ElectronsProducer>("electrons");
     const MuonsProducer& muons = producers.get<MuonsProducer>("muons");
     const JetsProducer& jets = producers.get<JetsProducer>("jets");
     return ((electrons.p4.size() + muons.p4.size() >= 2)  && jets.p4.size() >= 2);
 };
 
-bool HHMuElCategory::event_in_category_post_analyzers(const ProducersManager& producers, const AnalyzersManager& analyzers) const {
-    const std::vector<dilepton>& tempdiLeptons = getdiLeptons(analyzers);
+bool MuElCategory::event_in_category_post_analyzers(const ProducersManager& producers, const AnalyzersManager& analyzers) const {
+    const std::vector<Dilepton>& ll = getDileptons(analyzers);
     bool isMuEl = false;
-    for (unsigned int idilep = 0; idilep < tempdiLeptons.size(); idilep++) 
+    for (unsigned int idilep = 0; idilep < ll.size(); idilep++) 
     {
-        if (tempdiLeptons[idilep].isMuEl) isMuEl = true;
+        if (ll[idilep].isMuEl) isMuEl = true;
     }
-    const unsigned int numberOfJet = getNumberOfJet(analyzers);
-    return (isMuEl && numberOfJet >= 2);
+    const unsigned int nJets = getNJets(analyzers);
+    return (isMuEl && nJets >= 2);
 };
 
-void HHMuElCategory::register_cuts(CutManager& manager) {
+void MuElCategory::register_cuts(CutManager& manager) {
     manager.new_cut("ll_mass", "mll > 20");
     manager.new_cut("ll_mass_lowerZcut", "mll > 85");
     manager.new_cut("has_two_bJets", "nBJet >= 2");
 };
 
-void HHMuElCategory::evaluate_cuts_post_analyzers(CutManager& manager, const ProducersManager& producers, const AnalyzersManager& analyzers) const {
-    const std::vector<dilepton>& tempdiLeptons = getdiLeptons(analyzers);
-    for (unsigned int idilep = 0; idilep < tempdiLeptons.size(); idilep++) 
+void MuElCategory::evaluate_cuts_post_analyzers(CutManager& manager, const ProducersManager& producers, const AnalyzersManager& analyzers) const {
+    const std::vector<Dilepton>& ll = getDileptons(analyzers);
+    for (unsigned int idilep = 0; idilep < ll.size(); idilep++) 
     {
-        if ( tempdiLeptons[idilep].isMuEl ) {
-            if (  tempdiLeptons[idilep].p4.M() > m_mll_cut  ) manager.pass_cut("ll_mass");
-            if (  tempdiLeptons[idilep].p4.M() > m_mll_lowerZcut  ) manager.pass_cut("ll_mass_lowerZcut");
+        if ( ll[idilep].isMuEl ) {
+            if (  ll[idilep].p4.M() > m_mll_cut  ) manager.pass_cut("ll_mass");
+            if (  ll[idilep].p4.M() > m_mll_lowerZcut  ) manager.pass_cut("ll_mass_lowerZcut");
         }
     }
-    const unsigned int numberOfBJet = getNumberOfBJet(analyzers);
-    if ( numberOfBJet >=2 ) manager.pass_cut("has_two_bJets"); 
+    const unsigned int nBJets = getNBJets(analyzers);
+    if ( nBJets >=2 ) manager.pass_cut("has_two_bJets"); 
 }
 
